9db1ed7c9dada5333bb822871a58736fc2f41b0f
trevthedev777 <bloodsportrecords@gmail.com>
2022-11-12 22:11:18 +0000

    add templates and create furst developmemt view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}  # fills slug field automatically  # noqa
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
+ 
+
+# allows the admin to control the site backend
+# admin.site.register(Post)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']  # actions takes funcs as args
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post  # uses the Post Model
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'  # the template our view will render
+    paginate_by = 6   # limiting the amount of posts that show on a page
+
