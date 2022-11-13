8f9536e86685bd58d652f84eac9336177d64c4ef
trevthedev777 <bloodsportrecords@gmail.com>
2022-11-13 14:53:51 +0000

    add html templates for login and log out pages, add views and url paths

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,12 +41,24 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+# where the site redirecst after loginin /out
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
