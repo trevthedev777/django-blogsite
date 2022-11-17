b5eb6e08842017358c31e4e367ac086fdf19604c
trevthedev777 <bloodsportrecords@gmail.com>
2022-11-17 12:25:34 +0000

    Create env var to set debug to true in devlopment and fals when pushed to heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # this comes from the lib we installed above and put in our installed_apps section  # noqa
 STATIC_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
